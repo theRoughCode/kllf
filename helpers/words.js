94c1caa167afb5af51b5d5d49ae9b03e3954f692
raphael <raph.koh@gmail.com>
2017-05-13 16:07:26 -0400

    Add prediction

--- helpers/words.js
+++ helpers/words.js
@@ -10,7 +10,13 @@ var app = new Clarifai.App(
 function predict(img_64, callback) {
   app.models.predict(Clarifai.GENERAL_MODEL, img_64).then(
     res => {
-      return callback(res);
+      const outputs = [];
+      var concepts;
+      for (var i in response.outputs){
+        concepts = response.outputs[i].data.concepts;
+        var keywords = [];
+        concepts.forEach(concept => keywords.push([concept.name, concept.value]));
+      return callback(keywords);
     }, err => {
       console.error(console.error(err));
       return callback(null);
